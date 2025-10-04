d0a1cd45cee3fd242a0841673237538c5e8ee6a2
RuiNtD <RuiNtD@outlook.com>
2025-10-03 18:29:42 -0600

    Switch to Handlebars Template

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Brave Goggle to prioritize indie wikis (Based on Indie Wiki Buddy)
 
 [About this Goggle][about] | [Try this Goggle][try]
 
+## Strict Mode
+
+Alternative version that **only** shows indie wikis
+
+[About this Goggle][aboutS] | [Try this Goggle][tryS]
+
+[Indie Wiki Buddy]: https://getindie.wiki/
 [about]: https://search.brave.com/goggles/profile?goggles_id=https://raw.githubusercontent.com/RuiNtD/indie-wikis-goggle/refs/heads/main/out/indie_wikis.goggles
 [try]: https://search.brave.com/goggles?goggles_id=https://raw.githubusercontent.com/RuiNtD/indie-wikis-goggle/refs/heads/main/out/indie_wikis.goggles
-[Indie Wiki Buddy]: https://getindie.wiki/
+[aboutS]: https://search.brave.com/goggles/profile?goggles_id=https://raw.githubusercontent.com/RuiNtD/indie-wikis-goggle/refs/heads/main/out/strict.goggles
+[tryS]: https://search.brave.com/goggles?goggles_id=https://raw.githubusercontent.com/RuiNtD/indie-wikis-goggle/refs/heads/main/out/strict.goggles
--- src/build.ts
+++ src/build.ts
@@ -2,7 +2,12 @@ import $ from "@david/dax";
 import { iwbBase, iwbVersion } from "./const.ts";
 import * as z from "zod";
 import pMap from "p-map";
-import dedent from "dedent";
+import Handlebars from "handlebars";
+
+import goggleTemp from "./goggle.handlebars" with { type: "text" };
+import strictTemp from "./strict.handlebars" with { type: "text" };
+const goggleTemplate = Handlebars.compile(goggleTemp);
+const strictTemplate = Handlebars.compile(strictTemp);
 
 const Site = z.object({
   origins_label: z.string(),
@@ -13,50 +18,41 @@ const Site = z.object({
 const Sites = z.array(Site);
 async function getSites(lang: string) {
   return Sites.parse(
-    await $.request(`${iwbBase}/data/sites${lang}.json`).json()
+    await $.request(`${iwbBase}/data/sites${lang}.json`).json(),
   );
 }
 
 const LocaleMessages = z.record(z.string(), z.object({ message: z.string() }));
 async function getLangs() {
   const messages = LocaleMessages.parse(
-    await $.request(`${iwbBase}/_locales/en/messages.json`).json()
+    await $.request(`${iwbBase}/_locales/en/messages.json`).json(),
   );
-  return Object.fromEntries(
-    Object.entries(messages)
-      .filter(([k]) => k.startsWith("settingsLang") && k != "settingsLangAll")
-      .map(([k, v]) => [k.replace("settingsLang", ""), v.message])
-  );
+  return Object.entries(messages)
+    .filter(([k]) => k.startsWith("settingsLang") && k != "settingsLangAll")
+    .map(([k, v]) => [k.replace("settingsLang", ""), v.message]);
 }
 
-function formatBaseUrl(base: string) {
+function parseBaseUrl(base: string) {
   const parts = base.split("/");
-  if (parts.length == 1) return `$site=${base}`;
+  if (parts.length == 1) return { path: "", site: base };
   const path = parts.slice(1).join("/");
-  return `/${path}$site=${parts[0]}`;
+  return { path: `/${path}`, site: parts[0] };
 }
 
 const langs = await getLangs();
-const header = dedent`
-! name: Indie Wikis
-! description: Brave Goggle to prioritize indie wikis (Based on Indie Wiki Buddy ${iwbVersion})
-! public: true
-! author: RuiNtD
-! homepage: https://github.com/RuiNtD/indie-wikis-goggle
-! avatar: #005799
-! license: MIT
-`;
-const rules = await pMap(Object.entries(langs), async ([lang, langName]) => {
-  const header = `! ${langName}`;
-  const rules = (await getSites(lang)).map((site) =>
-    [
-      `! ${site.destination}`,
-      formatBaseUrl(site.destination_base_url),
-      ...site.origins.map((v) => `${formatBaseUrl(v.origin_base_url)},discard`),
-    ].join("\n")
-  );
-  return `${header}\n\n${rules.join("\n\n")}`;
-});
-await $.path("out/indie_wikis.goggles").writeText(
-  [header, ...rules].join("\n\n") + "\n"
-);
+const data = {
+  iwbVersion,
+  langs: await pMap(langs, async ([lang, name]) => ({
+    name,
+    sites: (await getSites(lang)).map((site) => ({
+      dest: {
+        name: site.destination,
+        ...parseBaseUrl(site.destination_base_url),
+      },
+      origins: site.origins.map((v) => parseBaseUrl(v.origin_base_url)),
+    })),
+  })),
+};
+
+await $.path("out/indie_wikis.goggles").writeText(goggleTemplate(data));
+await $.path("out/indie_wikis_strict.goggles").writeText(strictTemplate(data));
